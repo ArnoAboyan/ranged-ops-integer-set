3e446f3b5b79c1899fda59c39f45f331065061a5
Evgenii_Efimchik <evgenii_efimchik@epam.com>
2021-11-27 22:37:58 +0300

    test fix

--- src/test/java/com/epam/autotasks/collections/CodeComplianceTests.java
+++ src/test/java/com/epam/autotasks/collections/CodeComplianceTests.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CodeComplianceTests {
 
     @Test
-    void testMedianQueueAncestor() {
+    void testAncestor() {
         assertEquals(AbstractSet.class, RangedOpsIntegerSet.class.getSuperclass());
     }
 }
