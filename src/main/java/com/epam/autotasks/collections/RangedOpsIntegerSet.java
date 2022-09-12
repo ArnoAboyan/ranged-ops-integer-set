3e74bfd22af0f47761459b4a8e966d572d6dd513
withkeygames <withkeygames@gmail.com>
2022-09-12 15:25:59 +0300

    Solved

--- src/main/java/com/epam/autotasks/collections/RangedOpsIntegerSet.java
+++ src/main/java/com/epam/autotasks/collections/RangedOpsIntegerSet.java
@@ -1,35 +1,95 @@
 package com.epam.autotasks.collections;
 
-import java.util.AbstractSet;
-import java.util.Iterator;
+import java.util.*;
+
 
 class RangedOpsIntegerSet extends AbstractSet<Integer> {
 
+    public List<Integer> numbers = new ArrayList<>();
+
     public boolean add(int fromInclusive, int toExclusive) {
-        throw new UnsupportedOperationException();
+        boolean res = false;
+        for (int i = fromInclusive; i < toExclusive; i++) {
+            boolean result = add(i);
+            if (result && !res) res = true;
+        }
+        return res;
     }
 
     public boolean remove(int fromInclusive, int toExclusive) {
-        throw new UnsupportedOperationException();
+
+        Integer counter = 0;
+        for (int i = fromInclusive; i < toExclusive; i++) {
+
+            if (numbers.contains(i)) {
+                numbers.remove((Integer) i);
+                counter++;
+            }
+        }
+        return counter>0;
+    }
+
+    public boolean elementCheck(Integer number) {
+        for (Integer num : numbers) {
+            if (num.equals(number)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public boolean add(final Integer integer) {
-        throw new UnsupportedOperationException();
+
+        if (!elementCheck(integer)) {
+            numbers.add(integer);
+            Collections.sort(numbers);
+            return true;
+        }
+        return false;
     }
 
-    @Override
-    public boolean remove(final Object o) {
-        throw new UnsupportedOperationException();
-    }
+
 
     @Override
     public Iterator<Integer> iterator() {
-        throw new UnsupportedOperationException();
+
+        return  new IntegerIterator();
+
+    }
+
+
+    class IntegerIterator implements Iterator<Integer> {
+     int i = 0;
+        @Override
+        public boolean hasNext() {
+           return numbers.toArray().length > i;
+        }
+
+        @Override
+        public Integer next() {
+            Integer[] arr= numbers.toArray(new Integer[0]);
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return arr[i++];
+        }
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return numbers.size();
+    }
+
+
+    public static void main(String[] args) {
+        RangedOpsIntegerSet set = new RangedOpsIntegerSet();
+        set.add(11, 15);
+        set.add(7);
+        set.remove(12,14);
+        for (Integer num : set) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
